b71cfda2f692ac35d5ad20fe31c1e642b77a9066
aliaksandr.isayeu <aliaksandr.isayeu@epicgames.com>
2022-02-17 18:49:54 +0300

    More meaningful tests

--- solution.go
+++ solution.go
@@ -24,11 +24,11 @@ func CalcSquare(sideLen float64, sidesNum anotherInt) float64 {
 }
 
 func triangleArea(sideLen float64) float64 {
-	return math.Sqrt(0.75) * math.Pow(sideLen, 2)
+	return math.Sqrt(3) * math.Pow(sideLen, 2) / 4
 }
 func squareArea(sideLen float64) float64 {
 	return math.Pow(sideLen, 2)
 }
 func circleArea(sideLen float64) float64 {
-	return math.Pi * math.Pow(sideLen, 2)
+	return math.Pi * math.Pow(sideLen, 2) / 4
 }
--- solution_test.go
+++ solution_test.go
@@ -16,13 +16,13 @@ func TestCalcSquare(t *testing.T) {
 		want float64
 	}{
 		{"square", args{sideLen: 10.0, sidesNum: 4}, 100.0},
-		{"triangle", args{sideLen: 10.0, sidesNum: 3}, 86.60},
-		{"circle", args{sideLen: 10.0, sidesNum: 0}, 314.16},
+		{"triangle", args{sideLen: 2.0, sidesNum: 3}, math.Sqrt(3)},
+		{"circle", args{sideLen: 2.0, sidesNum: 0}, math.Pi},
 		{"wrong sides", args{sideLen: 10.0, sidesNum: 5}, 0.0},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := CalcSquare(tt.args.sideLen, tt.args.sidesNum); math.Round(got*100)/100 != tt.want {
+			if got := CalcSquare(tt.args.sideLen, tt.args.sidesNum); got != tt.want {
 				t.Errorf("CalcSquare() = %v, want %v", got, tt.want)
 			}
 		})
